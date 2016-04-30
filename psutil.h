c2d6d86d9681234eb42fc95f1740ef2d76fdd69c
Reuben Thomas <rrt@sc3d.org>
2016-05-01 00:36:20 +0100

    Add my first copyright year to COPYRIGHT_STRING

--- psutil.h
+++ psutil.h
@@ -48,4 +48,4 @@ extern const char *argerr_message;
 
 /* Useful macros */
 #define MIN(x,y) ((x) > (y) ? (y) : (x))
-#define COPYRIGHT_STRING "(c) Reuben Thomas <rrt@sc3d.org> 2016\n(c) Angus J. C. Duggan 1991-1997\nSee file LICENSE for details.\n"
+#define COPYRIGHT_STRING "(c) Reuben Thomas <rrt@sc3d.org> 2012-2016\n(c) Angus J. C. Duggan 1991-1997\nSee file LICENSE for details.\n"
