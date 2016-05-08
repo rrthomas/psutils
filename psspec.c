rrthomas/psutils

commit 7a537f7219e16a1afd8ceac960e0847ec6c01411

Simplify pstops
Add writestringf which outputs a formatted string

psspec.c
@@ -92,50 +92,44 @@ double singledimen(char *str)
    return (num);
 }
 
-static const char *prologue[] = { /* PStoPS procset */
+static const char *prologue = /* PStoPS procset */
    /* Wrap these up with our own versions.  We have to  */
-   "userdict begin",
-   "[/showpage/erasepage/copypage]{dup where{pop dup load",
-   " type/operatortype eq{ /PStoPSenablepage cvx 1 index"
-   " load 1 array astore cvx {} bind /ifelse cvx 4 array"
-   " astore cvx def}{pop}ifelse}{pop}ifelse}forall"
-   " /PStoPSenablepage true def",
-   "[/letter/legal/executivepage/a4/a4small/b5/com10envelope",	/* nullify */
-   " /monarchenvelope/c5envelope/dlenvelope/lettersmall/note",	/* paper */
-   " /folio/quarto/a5]{dup where{dup wcheck{exch{}put}",	/* operators */
-   " {pop{}def}ifelse}{pop}ifelse}forall",
-   "/setpagedevice {pop}bind 1 index where{dup wcheck{3 1 roll put}",
-   " {pop def}ifelse}{def}ifelse",
-   "/PStoPSmatrix matrix currentmatrix def",
-   "/PStoPSxform matrix def/PStoPSclip{clippath}def",
-   "/defaultmatrix{PStoPSmatrix exch PStoPSxform exch concatmatrix}bind def",
-   "/initmatrix{matrix defaultmatrix setmatrix}bind def",
-   "/initclip[{matrix currentmatrix PStoPSmatrix setmatrix",
-   " [{currentpoint}stopped{$error/newerror false put{newpath}}",
-   " {/newpath cvx 3 1 roll/moveto cvx 4 array astore cvx}ifelse]",
-   " {[/newpath cvx{/moveto cvx}{/lineto cvx}",
-   " {/curveto cvx}{/closepath cvx}pathforall]cvx exch pop}",
-   " stopped{$error/errorname get/invalidaccess eq{cleartomark",
-   " $error/newerror false put cvx exec}{stop}ifelse}if}bind aload pop",
-   " /initclip dup load dup type dup/operatortype eq{pop exch pop}",
-   " {dup/arraytype eq exch/packedarraytype eq or",
-   "  {dup xcheck{exch pop aload pop}{pop cvx}ifelse}",
-   "  {pop cvx}ifelse}ifelse",
-   " {newpath PStoPSclip clip newpath exec setmatrix} bind aload pop]cvx def",
-   "/initgraphics{initmatrix newpath initclip 1 setlinewidth",
-   " 0 setlinecap 0 setlinejoin []0 setdash 0 setgray",
-   " 10 setmiterlimit}bind def",
-   "end",
-   NULL
-   };
+"userdict begin\
+[/showpage/erasepage/copypage]{dup where{pop dup load\
+ type/operatortype eq{ /PStoPSenablepage cvx 1 index\
+ load 1 array astore cvx {} bind /ifelse cvx 4 array\
+ astore cvx def}{pop}ifelse}{pop}ifelse}forall\
+ /PStoPSenablepage true def\
+[/letter/legal/executivepage/a4/a4small/b5/com10envelope%nullify\
+ /monarchenvelope/c5envelope/dlenvelope/lettersmall/note%paper\
+ /folio/quarto/a5]{dup where{dup wcheck{exch{}put}%operators\
+ {pop{}def}ifelse}{pop}ifelse}forall\
+/setpagedevice {pop}bind 1 index where{dup wcheck{3 1 roll put}\
+ {pop def}ifelse}{def}ifelse\
+/PStoPSmatrix matrix currentmatrix def\
+/PStoPSxform matrix def/PStoPSclip{clippath}def\
+/defaultmatrix{PStoPSmatrix exch PStoPSxform exch concatmatrix}bind def\
+/initmatrix{matrix defaultmatrix setmatrix}bind def\
+/initclip[{matrix currentmatrix PStoPSmatrix setmatrix\
+ [{currentpoint}stopped{$error/newerror false put{newpath}}\
+ {/newpath cvx 3 1 roll/moveto cvx 4 array astore cvx}ifelse]\
+ {[/newpath cvx{/moveto cvx}{/lineto cvx}\
+ {/curveto cvx}{/closepath cvx}pathforall]cvx exch pop}\
+ stopped{$error/errorname get/invalidaccess eq{cleartomark\
+ $error/newerror false put cvx exec}{stop}ifelse}if}bind aload pop\
+ /initclip dup load dup type dup/operatortype eq{pop exch pop}\
+ {dup/arraytype eq exch/packedarraytype eq or\
+  {dup xcheck{exch pop aload pop}{pop cvx}ifelse}\
+  {pop cvx}ifelse}ifelse\
+ {newpath PStoPSclip clip newpath exec setmatrix} bind aload pop]cvx def\
+/initgraphics{initmatrix newpath initclip 1 setlinewidth\
+ 0 setlinecap 0 setlinejoin []0 setdash 0 setgray\
+ 10 setmiterlimit}bind def\
+end\n";
 
 void pstops(int modulo, int pps, int nobind, PageSpec *specs, double draw, off_t *ignorelist)
 {
-   int thispg, maxpage;
-   int pageindex = 0;
-   const char **pro;
-
-   maxpage = ((pages+modulo-1)/modulo)*modulo;
+   int maxpage = ((pages+modulo-1)/modulo)*modulo;
 
    /* rearrange pages: doesn't cope properly with loaded definitions */
    writeheadermedia((maxpage/modulo)*pps, ignorelist, width, height);
@@ -143,26 +137,22 @@ void pstops(int modulo, int pps, int nobind, PageSpec *specs, double draw, off_t
    if (nobind)
       writestring("-nobind");
    writestring(" 1 15\n");
-   for (pro = prologue; *pro; pro++) {
-      writestring(*pro);
-      writestring("\n");
-   }
+   writestring(prologue);
    if (nobind) /* desperation measures */
       writestring("/bind{}def\n");
    writestring("%%EndProcSet\n");
    /* save transformation from original to current matrix */
    if (writepartprolog()) {
-      writestring("userdict/PStoPSxform PStoPSmatrix matrix currentmatrix\n");
-      writestring(" matrix invertmatrix matrix concatmatrix\n");
-      writestring(" matrix invertmatrix put\n");
+      writestring("userdict/PStoPSxform PStoPSmatrix matrix currentmatrix\
+ matrix invertmatrix matrix concatmatrix\
+ matrix invertmatrix put\n");
    }
    writesetup();
-   for (thispg = 0; thispg < maxpage; thispg += modulo) {
+   int pageindex = 0;
+   for (int thispg = 0; thispg < maxpage; thispg += modulo) {
       int add_last = 0;
-      PageSpec *ps;
-      for (ps = specs; ps != NULL; ps = ps->next) {
+      for (PageSpec *ps = specs; ps != NULL; ps = ps->next) {
 	 int actualpg;
-	 int add_next = ((ps->flags & ADD_NEXT) != 0);
 	 if (ps->flags & REVERSED)
 	    actualpg = maxpage-thispg-modulo+ps->pageno;
 	 else
@@ -174,12 +164,7 @@ void pstops(int modulo, int pps, int nobind, PageSpec *specs, double draw, off_t
 	    char *eob = pagelabel;
 	    char sep = '(';
 	    do {
-	       *eob++ = sep;
-	       if (np->flags & REVERSED)
-		  sprintf(eob, "%d", maxpage-thispg-modulo+np->pageno);
-	       else
-		  sprintf(eob, "%d", thispg+np->pageno);
-	       eob = eob + strlen(eob);
+               eob += sprintf(eob, "%c%d", sep, (np->flags & REVERSED) ? maxpage-thispg-modulo+np->pageno : thispg+np->pageno);
 	       sep = ',';
 	    } while ((np->flags & ADD_NEXT) && (np = np->next));
 	    strcpy(eob, ")");
@@ -187,53 +172,36 @@ void pstops(int modulo, int pps, int nobind, PageSpec *specs, double draw, off_t
 	 }
 	 writestring("userdict/PStoPSsaved save put\n");
 	 if (ps->flags & GSAVE) {
-	    char buffer[BUFSIZ];
 	    writestring("PStoPSmatrix setmatrix\n");
-	    if (ps->flags & OFFSET) {
-	       sprintf(buffer, "%f %f translate\n", ps->xoff, ps->yoff);
-	       writestring(buffer);
-	    }
-	    if (ps->flags & ROTATE) {
-	       sprintf(buffer, "%d rotate\n", ps->rotate);
-	       writestring(buffer);
-	    }
-	    if (ps->flags & HFLIP) {
-	       sprintf(buffer, "[ -1 0 0 1 %f 0 ] concat\n", width*ps->scale);
-	       writestring(buffer);
-	    }
-	    if (ps->flags & VFLIP) {
-	       sprintf(buffer, "[ 1 0 0 -1 0 %f ] concat\n", height*ps->scale);
-	       writestring(buffer);
-	    }
-	    if (ps->flags & SCALE) {
-	       sprintf(buffer, "%f dup scale\n", ps->scale);
-	       writestring(buffer);
-	    }
+	    if (ps->flags & OFFSET)
+	       writestringf("%f %f translate\n", ps->xoff, ps->yoff);
+	    if (ps->flags & ROTATE)
+	       writestringf("%d rotate\n", ps->rotate);
+	    if (ps->flags & HFLIP)
+	       writestringf("[ -1 0 0 1 %f 0 ] concat\n", width*ps->scale);
+	    if (ps->flags & VFLIP)
+	       writestringf("[ 1 0 0 -1 0 %f ] concat\n", height*ps->scale);
+	    if (ps->flags & SCALE)
+	       writestringf("%f dup scale\n", ps->scale);
 	    writestring("userdict/PStoPSmatrix matrix currentmatrix put\n");
 	    if (width > 0 && height > 0) {
-	       writestring("userdict/PStoPSclip{0 0 moveto\n");
-	       sprintf(buffer, " %f 0 rlineto 0 %f rlineto -%f 0 rlineto\n",
-		       width, height, width);
-	       writestring(buffer);
-	       writestring(" closepath}put initclip\n");
-	       if (draw > 0) {
-		  sprintf(buffer, "gsave clippath 0 setgray %f setlinewidth stroke grestore\n", draw);
-		  writestring(buffer);
-	       }
+	       writestringf("userdict/PStoPSclip{0 0 moveto\
+ %f 0 rlineto 0 %f rlineto -%f 0 rlineto\
+ closepath}put initclip\n", width, height, width);
+	       if (draw > 0)
+		  writestringf("gsave clippath 0 setgray %f setlinewidth stroke grestore\n", draw);
 	    }
 	 }
-	 if (add_next)
+	 if ((add_last = (ps->flags & ADD_NEXT) != 0))
 	    writestring("/PStoPSenablepage false def\n");
 	 if (actualpg < pages) {
 	    writepagesetup();
 	    writestring("PStoPSxform concat\n");
 	    writepagebody(actualpg);
-	 } else {
-	    writestring("PStoPSxform concat\n");
-	    writestring("showpage\n");
-	 }
+	 } else
+	    writestring("PStoPSxform concat\
+showpage\n");
 	 writestring("PStoPSsaved restore\n");
-	 add_last = add_next;
       }
    }
    writetrailer();

psnup.c
@@ -173,44 +173,42 @@ main(int argc, char *argv[])
    /* Finding the best layout is an optimisation problem. We try all of the
     * combinations of width*height in both normal and rotated form, and
     * minimise the wasted space. */
-   {
-      double best = tolerance;
-      for (long hor = 1; hor; hor = nextdiv(hor, nup)) {
-	 long ver = nup/hor;
-	 /* try normal orientation first */
-	 double scl = MIN(pphgt/(height*ver), ppwid/(width*hor));
-	 double optim = (ppwid-scl*width*hor)*(ppwid-scl*width*hor) +
-	    (pphgt-scl*height*ver)*(pphgt-scl*height*ver);
-	 if (optim < best) {
-	    best = optim;
-	    /* recalculate scale to allow for internal borders */
-	    scale = MIN((pphgt-2*border*ver)/(height*ver),
-			(ppwid-2*border*hor)/(width*hor));
-	    hshift = (ppwid/hor - width*scale)/2;
-	    vshift = (pphgt/ver - height*scale)/2;
-	    horiz = hor; vert = ver;
-	    rotate = flip;
-	 }
-	 /* try rotated orientation */
-	 scl = MIN(pphgt/(width*hor), ppwid/(height*ver));
-	 optim = (pphgt-scl*width*hor)*(pphgt-scl*width*hor) +
-	    (ppwid-scl*height*ver)*(ppwid-scl*height*ver);
-	 if (optim < best) {
-	    best = optim;
-	    /* recalculate scale to allow for internal borders */
-	    scale = MIN((pphgt-2*border*hor)/(width*hor),
-			(ppwid-2*border*ver)/(height*ver));
-	    hshift = (ppwid/ver - height*scale)/2;
-	    vshift = (pphgt/hor - width*scale)/2;
-	    horiz = ver; vert = hor;
-	    rotate = !flip;
-	 }
-      }
-
-      /* fail if nothing better than worst tolerance was found */
-      if (best == tolerance)
-	 die("can't find acceptable layout for %d-up", nup);
+   double best = tolerance;
+   for (long hor = 1; hor; hor = nextdiv(hor, nup)) {
+     long ver = nup/hor;
+     /* try normal orientation first */
+     double scl = MIN(pphgt/(height*ver), ppwid/(width*hor));
+     double optim = (ppwid-scl*width*hor)*(ppwid-scl*width*hor) +
+       (pphgt-scl*height*ver)*(pphgt-scl*height*ver);
+     if (optim < best) {
+       best = optim;
+       /* recalculate scale to allow for internal borders */
+       scale = MIN((pphgt-2*border*ver)/(height*ver),
+                   (ppwid-2*border*hor)/(width*hor));
+       hshift = (ppwid/hor - width*scale)/2;
+       vshift = (pphgt/ver - height*scale)/2;
+       horiz = hor; vert = ver;
+       rotate = flip;
+     }
+     /* try rotated orientation */
+     scl = MIN(pphgt/(width*hor), ppwid/(height*ver));
+     optim = (pphgt-scl*width*hor)*(pphgt-scl*width*hor) +
+       (ppwid-scl*height*ver)*(ppwid-scl*height*ver);
+     if (optim < best) {
+       best = optim;
+       /* recalculate scale to allow for internal borders */
+       scale = MIN((pphgt-2*border*hor)/(width*hor),
+                   (ppwid-2*border*ver)/(height*ver));
+       hshift = (ppwid/ver - height*scale)/2;
+       vshift = (pphgt/hor - width*scale)/2;
+       horiz = ver; vert = hor;
+       rotate = !flip;
+     }
    }
+
+   /* fail if nothing better than worst tolerance was found */
+   if (best == tolerance)
+     die("can't find acceptable layout for %d-up", nup);
 
    if (flip) {	/* swap width & height for clipping */
       double tmp = width;

psutil.c
@@ -17,6 +17,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <stdio.h>
+#include <stdarg.h>
 
 #include "progname.h"
 #include "xvasprintf.h"
@@ -329,8 +330,15 @@ void seekpage(int p)
  * of bytes written */
 void writestring(const char *s)
 {
-   fputs(s, outfile);
-   bytes += strlen(s);
+  writestringf("%s", s);
+}
+
+void writestringf(const char *f, ...)
+{
+  va_list ap;
+  va_start(ap, f);
+  bytes += vfprintf(outfile, f, ap);
+  va_end(ap);
 }
 
 /* write page comment */

psutil.h
@@ -32,6 +32,7 @@ extern void writetrailer(void);
 extern void writeemptypage(void);
 extern void scanpages(off_t *sizeheaders);
 extern void writestring(const char *s);
+extern void writestringf(const char *f, ...);
 
 /* These variables are exported to the client program */
 extern int pages;

commit 8fd5fcb0ba551aeeb69f37621b4e22e0d48805d3

psspec.c: restore missing line endings in prologue
Broken in commit 7a537f7

psspec.c
@@ -109,37 +109,37 @@ int page_index_to_real_page(PageSpec *ps, int maxpage, int modulo, int signature
 
 static const char *prologue = /* PStoPS procset */
    /* Wrap these up with our own versions.  We have to  */
-"userdict begin\
-[/showpage/erasepage/copypage]{dup where{pop dup load\
- type/operatortype eq{ /PStoPSenablepage cvx 1 index\
- load 1 array astore cvx {} bind /ifelse cvx 4 array\
- astore cvx def}{pop}ifelse}{pop}ifelse}forall\
- /PStoPSenablepage true def\
-[/letter/legal/executivepage/a4/a4small/b5/com10envelope%nullify\
- /monarchenvelope/c5envelope/dlenvelope/lettersmall/note%paper\
- /folio/quarto/a5]{dup where{dup wcheck{exch{}put}%operators\
- {pop{}def}ifelse}{pop}ifelse}forall\
-/setpagedevice {pop}bind 1 index where{dup wcheck{3 1 roll put}\
- {pop def}ifelse}{def}ifelse\
-/PStoPSmatrix matrix currentmatrix def\
-/PStoPSxform matrix def/PStoPSclip{clippath}def\
-/defaultmatrix{PStoPSmatrix exch PStoPSxform exch concatmatrix}bind def\
-/initmatrix{matrix defaultmatrix setmatrix}bind def\
-/initclip[{matrix currentmatrix PStoPSmatrix setmatrix\
- [{currentpoint}stopped{$error/newerror false put{newpath}}\
- {/newpath cvx 3 1 roll/moveto cvx 4 array astore cvx}ifelse]\
- {[/newpath cvx{/moveto cvx}{/lineto cvx}\
- {/curveto cvx}{/closepath cvx}pathforall]cvx exch pop}\
- stopped{$error/errorname get/invalidaccess eq{cleartomark\
- $error/newerror false put cvx exec}{stop}ifelse}if}bind aload pop\
- /initclip dup load dup type dup/operatortype eq{pop exch pop}\
- {dup/arraytype eq exch/packedarraytype eq or\
-  {dup xcheck{exch pop aload pop}{pop cvx}ifelse}\
-  {pop cvx}ifelse}ifelse\
- {newpath PStoPSclip clip newpath exec setmatrix} bind aload pop]cvx def\
-/initgraphics{initmatrix newpath initclip 1 setlinewidth\
- 0 setlinecap 0 setlinejoin []0 setdash 0 setgray\
- 10 setmiterlimit}bind def\
+"userdict begin\n\
+[/showpage/erasepage/copypage]{dup where{pop dup load\n\
+ type/operatortype eq{ /PStoPSenablepage cvx 1 index\n\
+ load 1 array astore cvx {} bind /ifelse cvx 4 array\n\
+ astore cvx def}{pop}ifelse}{pop}ifelse}forall\n\
+ /PStoPSenablepage true def\n\
+[/letter/legal/executivepage/a4/a4small/b5/com10envelope%nullify\n\
+ /monarchenvelope/c5envelope/dlenvelope/lettersmall/note%paper\n\
+ /folio/quarto/a5]{dup where{dup wcheck{exch{}put}%operators\n\
+ {pop{}def}ifelse}{pop}ifelse}forall\n\
+/setpagedevice {pop}bind 1 index where{dup wcheck{3 1 roll put}\n\
+ {pop def}ifelse}{def}ifelse\n\
+/PStoPSmatrix matrix currentmatrix def\n\
+/PStoPSxform matrix def/PStoPSclip{clippath}def\n\
+/defaultmatrix{PStoPSmatrix exch PStoPSxform exch concatmatrix}bind def\n\
+/initmatrix{matrix defaultmatrix setmatrix}bind def\n\
+/initclip[{matrix currentmatrix PStoPSmatrix setmatrix\n\
+ [{currentpoint}stopped{$error/newerror false put{newpath}}\n\
+ {/newpath cvx 3 1 roll/moveto cvx 4 array astore cvx}ifelse]\n\
+ {[/newpath cvx{/moveto cvx}{/lineto cvx}\n\
+ {/curveto cvx}{/closepath cvx}pathforall]cvx exch pop}\n\
+ stopped{$error/errorname get/invalidaccess eq{cleartomark\n\
+ $error/newerror false put cvx exec}{stop}ifelse}if}bind aload pop\n\
+ /initclip dup load dup type dup/operatortype eq{pop exch pop}\n\
+ {dup/arraytype eq exch/packedarraytype eq or\n\
+  {dup xcheck{exch pop aload pop}{pop cvx}ifelse}\n\
+  {pop cvx}ifelse}ifelse\n\
+ {newpath PStoPSclip clip newpath exec setmatrix} bind aload pop]cvx def\n\
+/initgraphics{initmatrix newpath initclip 1 setlinewidth\n\
+ 0 setlinecap 0 setlinejoin []0 setdash 0 setgray\n\
+ 10 setmiterlimit}bind def\n\
 end\n";
 
 void pstops(int signature, int modulo, int pps, int nobind, PageSpec *specs, double draw, off_t *ignorelist)