047fc387967b613f4c533688dd48c67f5afe0df7
Reuben Thomas <rrt@sc3d.org>
2023-01-31 21:42:49 +0000

    PSUtils.pm: remove some redundant quotes

--- PSUtils.pm
+++ PSUtils.pm
@@ -73,7 +73,7 @@ sub paper {
 sub paper_size {
   my ($paper_name) = @_;
   $paper_name = paper([]) unless defined($paper_name);
-  my $dimensions = paper(["--unit=pt", "$paper_name"], 1) or return;
+  my $dimensions = paper(["--unit=pt", $paper_name], 1) or return;
   $dimensions =~ / ([.0-9]+)x([.0-9]+) pt$/;
   my $old_locale = setlocale(LC_ALL);
   setlocale(LC_ALL, "");
