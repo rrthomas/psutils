6cdb2e5c8504a6bd81ad4043f3a4868267c19d72
Reuben Thomas <rrt@sc3d.org>
2023-04-19 21:20:16 +0100

    setup.py: ignore a type warning

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from build_manpages.build_manpages import build_manpages, get_install_cmd, get_b
 from setuptools import setup
 
 setup(
-    cmdclass={
+    cmdclass={ # type: ignore
         'build_manpages': build_manpages,
         'build_py': get_build_py_cmd(),
         'install': get_install_cmd(),
