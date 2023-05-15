65fe7a1c27b712c2ef6d6d4eff1939307a89aeb4
Reuben Thomas <rrt@sc3d.org>
2023-05-15 05:53:47 +0300

    setup.py: add to linting, and fix errors

--- setup.py
+++ setup.py
@@ -1,17 +1,21 @@
-from build_manpages.build_manpages import build_manpages, get_install_cmd, get_build_py_cmd # type: ignore
+import subprocess
+from build_manpages.build_manpages import (  # type: ignore
+    build_manpages,
+    get_install_cmd,
+    get_build_py_cmd,
+)
 from setuptools import setup
 
 setup(
     cmdclass={
-        'build_manpages': build_manpages,
-        'build_py': get_build_py_cmd(),
-        'install': get_install_cmd(),
+        "build_manpages": build_manpages,
+        "build_py": get_build_py_cmd(),
+        "install": get_install_cmd(),
     }
 )
 
 # Test we have 'paper' (from libpaper) installed
-import subprocess
 try:
-    subprocess.check_output(['paper'])
-except OSError:
-    raise SystemExit('psutils needs libpaper >= 2 to work')
+    subprocess.check_output(["paper"])
+except OSError as exc:
+    raise SystemExit("psutils needs libpaper >= 2 to work") from exc
